f2ca3f9d853d11487870beb72de08f80ac2670fd
Mazen G <mazenGharbi@gmail.com>
2016-01-25 17:57:37 +0100

    Ajout du Loading

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,24 @@
 window.onload = function () {
-    main();
+    setTimeout(main, 1000); // Petit délai pour voir le loading
 };
 
 function main() {
-    var appli = new App();        
+    // On lance le loading dès le chargement de la page
+    var loading = new Loading();
+    loading.chargerImages(); // Charger sons et images
+    loading.chargerSons();
+
+    /* On affiche le loading tant que toutes les images n'ont pas été chargées */
+    var intervalLoading = setInterval(function () {
+        if (loading.chargementImageTermine && loading.chargementSonTermine) {
+            // Quand tout est chargé, on supprime l'intervale permettant de savoir si le loading est finie, on retire l'image de loading et on lance le jeu
+            clearInterval(intervalLoading);
+            
+            var loadingDOM = document.getElementById("loading").style.display = "none";
+            loadingDOM;
+            
+            var appli = new App();
+        }
+    }, 200);
+    
 }
