340ec5b5cd2412f5b74a5a565a3d834975026ede
Camille Bovin <camille.bovin23@gmail.com>
2016-02-15 15:14:14 +0100

    Mise en place de la base de données

--- db.js
+++ db.js
@@ -1,62 +1,22 @@
 var mongoose = require( 'mongoose' );
 var Schema   = mongoose.Schema;
 
-/*var Todo = new Schema({
-    user_id    : String,
-    content    : String,
-    updated_at : Date
-});
-*/
-//TODO AJOUTER SCHEMA JOUEUR ETC
 
-var diagramme = new Schema ({
-    cells : [
-	{
-	    type: { type: String },
-	    size :{
-			width : Number,
-			height : Number
-	    },
-	    position :
-	    {
-			x : Number,//N
-			y : Number//N
-	    },
-
-	    angle : Number,//N
-	    id : String,
-	    z : Number,//N
-	    attrs : {
-	    },
-	    source: {
-	    	id: String
-	    },
-	    target: {
-	    	id: String
-	    }
-	   
-	    //userName : { type: Schema.Types.ObjectId, ref: 'User' }
-	   
-	}],
-	 userName : { type: Schema.Types.ObjectId, ref: 'User' },
-	 diagrammeName: String
-});
-
-var User =  new Schema ({
-		mail : {
+var Joueur =  new Schema ({
+		pseudo : {
 		type : String,
 		required : true,
 		unique : true},
-		password : {
+		highScore : {
 		type : String,
-		required : true},
-		//diagramme : [{ type: Schema.Types.ObjectId, ref: 'diagramme' }]
+		required : false},
+		ghost: [String],
 		
 });
 
-mongoose.model('diagramme', diagramme);
-mongoose.model('User', User);
+
+mongoose.model('Joueur', Joueur);
 //mongoose.model( 'Todo', Todo );
 //mongoose.connect( 'mongodb://localhost/zzzzzzzzzzzzzzzz' );
-//mongoose.connect( 'mongodb://127.0.0.1/umlDB' );
-mongoose.connect( 'mongodb://localhost/umlDB');
+mongoose.connect( 'mongodb://127.0.0.1/game' );
+//mongoose.connect( 'mongodb://localhost/umlDB');
