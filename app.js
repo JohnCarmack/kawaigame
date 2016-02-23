59c2498652bbe3636c9fdcb51f72d044f3672a87
Chaïb <faouzy.chaib@accenture.com>
2016-02-23 15:40:31 +0100

    Possible de rejoindre une room, need more work

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var router       = express.Router();
 
 // usernames which are currently connected to the chat
 var usernames = {};
+
 var listOfPlayers = {};
 var Joueur = mongoose.model('Joueur','joueur');
 
@@ -91,75 +92,81 @@ app.post('/newJoueur', function (req, res){
 
 var io = require('socket.io')(server);
 
-// usernames which are currently connected to the chat  
-var usernames = {};  
-var listOfPlayers = {};  
-  
-io.sockets.on('connection', function (socket) {  
-  
-    // when the client emits 'sendchat', this listens and executes  
-    socket.on('sendchat', function (data) {  
-        // we tell the client to execute 'updatechat' with 2 parameters 
-        console.log("dans le sendchat") 
-        console.log(data);
-        io.sockets.emit('updatechat', socket.username, data);  
-    });  
+// usernames which are currently connected to the chat
+var usernames = {};
+var listOfPlayers = {};
+var rooms=["room1", "room2"];
+console.log(rooms[1]);
+io.sockets.on('connection', function (socket) {
 
-    // when the client emits 'adduser', this listens and executes  
-    socket.on('adduser', function(username){
-        // we store the username in the socket session for this client  
-        // the 'socket' variable is unique for each client connected,  
-        // so we can use it as a sort of HTTP session  
-        socket.username = username;  
-        // add the client's username to the global list  
-        // similar to usernames.michel = 'michel', usernames.toto = 'toto'  
-        usernames[username] = username;  
-        // echo to the current client that he is connecter  
-        socket.emit('updatechat', 'SERVER', 'you have connected');  
-        // echo to all client except current, that a new person has connected  
-        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');  
-        // tell all clients to update the list of users on the GUI  
-        io.sockets.emit('updateusers', usernames);  
-  
-        // Create a new player and store his position too... for that  
-        // we have an object that is a "list of players" in that form  
-        // listOfPlayer = {'michel':{'x':0, 'y':0, 'v':0},   
-        //                          john:{'x':10, 'y':10, 'v':0}}  
-        // for this example we have x, y and v for speed... ? 
-        var player = {'x':0, 'y':0, 'v':4};
-        listOfPlayers[username] = player;  
-        console.log("user created : "+username);
-        io.sockets.emit('updatePlayers',listOfPlayers);  
+    // when the client emits 'sendchat', this listens and executes
+    socket.on('sendchat', function (data) {
+        // we tell the client to execute 'updatechat' with 2 parameters
+        console.log("dans le sendchat")
+        console.log(data);
+        io.sockets.emit('updatechat', socket.username, data);
     });
 
-    
+    // when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function(username){
+        // we store the username in the socket session for this client
+        // the 'socket' variable is unique for each client connected,
+        // so we can use it as a sort of HTTP session
+        socket.username = username;
+        // add the client's username to the global list
+        // similar to usernames.michel = 'michel', usernames.toto = 'toto'
+        usernames[username] = username;
+        // echo to the current client that he is connecter
+        socket.emit('updatechat', 'SERVER', 'you have connected');
+        // echo to all client except current, that a new person has connected
+        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+        // tell all clients to update the list of users on the GUI
+        io.sockets.emit('updateusers', usernames);
+        defaultRoom = rooms[0];
+
+        socket.join(defaultRoom);
+
+        io.in(defaultRoom).emit('updateroom',defaultRoom);
+        io.in(rooms[1]).emit('updateroom', rooms[1]);
+        // Create a new player and store his position too... for that
+        // we have an object that is a "list of players" in that form
+        // listOfPlayer = {'michel':{'x':0, 'y':0, 'v':0},
+        //                          john:{'x':10, 'y':10, 'v':0}}
+        // for this example we have x, y and v for speed... ?
+        var player = {'x':0, 'y':0, 'v':4};
+        listOfPlayers[username] = player;
+        console.log("user created : "+username);
+        io.sockets.emit('updatePlayers',listOfPlayers);
+    });
+
+
 
     //when a player moves
-    socket.on('sendpos', function (newPos) {  
-        // we tell the client to execute 'updatepos' with 2 parameters  
-        //console.log("recu sendPos : ");  
-        socket.broadcast.emit('updatepos', socket.username, newPos);  
-    });  
-  
-    // when the user disconnects.. perform this  
-    socket.on('disconnect', function(){  
-        // remove the username from global usernames list  
-        delete usernames[socket.username];  
-                // update list of users in chat, client-side  
-        io.sockets.emit('updateusers', usernames);  
-  
-        // Remove the player too  
-        delete listOfPlayers[socket.username];        
-        io.sockets.emit('updatePlayers',listOfPlayers);  
-          
-        // echo globally that this client has left  
-        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');  
-    }); 
+    socket.on('sendpos', function (newPos) {
+        // we tell the client to execute 'updatepos' with 2 parameters
+        //console.log("recu sendPos : ");
+        socket.broadcast.emit('updatepos', socket.username, newPos);
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function(){
+        // remove the username from global usernames list
+        delete usernames[socket.username];
+                // update list of users in chat, client-side
+        io.sockets.emit('updateusers', usernames);
+
+        // Remove the player too
+        delete listOfPlayers[socket.username];
+        io.sockets.emit('updatePlayers',listOfPlayers);
+
+        // echo globally that this client has left
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+    });
 
     // when the game starts
-    socket.on('sendStartGame', function (lvl) { 
-        // we tell the client to execute 'updatechat' with 2 parameters 
+    socket.on('sendStartGame', function (lvl) {
+        // we tell the client to execute 'updatechat' with 2 parameters
         console.log("on commence le jeu")
-        io.sockets.emit('startGame', lvl, listOfPlayers);  
-    });   
-});  
+        io.sockets.emit('startGame', lvl, listOfPlayers);
+    });
+});
