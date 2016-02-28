e1581dc8dac66e760e0dc5b2d3f84023919e0271
Camille Bovin <camille.bovin23@gmail.com>
2016-02-28 12:15:07 +0100

    Choix du serveur

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,19 @@ $.ajax({url: "/rooms", success: function(result){
 		console.log(result[i]);
 	 document.getElementById('serveur').innerHTML +=
 			  "<option>"+result[i]+"</option>";
+			  
+	document.getElementById('rooms').innerHTML +=
+			  "<option>"+result[i]+"</option>";
 	}
               
 }});
+
+function FirstRoom(){
+	var roomBefore = document.getElementById('rooms').value;
+	var room = document.getElementById('serveur').value;
+	if(roomBefore !== room){
+	document.getElementById('rooms').value = room;
+	document.getElementById('switchRoom').click();
+	}
+	document.getElementById('CloseServeurs').click();
+}
